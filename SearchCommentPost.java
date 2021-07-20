ebd8eb1a7d49f31c359b93a7c8d04164fd064447
OliverandShi <87618937+OliverandShi@users.noreply.github.com>
2021-07-19 21:07:26 -0400

    Add files via upload

--- SearchCommentPost.java
+++ SearchCommentPost.java
@@ -1,4 +1,4 @@
-import java.io.*;
+import javax.xml.stream.events.Comment;
 import java.util.ArrayList;
 import java.util.Scanner;
 /**
@@ -12,16 +12,17 @@ import java.util.Scanner;
 
 public class SearchCommentPost {
     private ArrayList<Post> posts = new ArrayList<Post>();
+    private ArrayList<Comment> comments = new ArrayList<Comment>();
     Scanner scan = new Scanner(System.in);
 
-    public SearchCommentPost(ArrayList<Post> posts) {
+    public SearchCommentPost(ArrayList<Post> posts, ArrayList<Comment> comments) {
         this.posts = posts;
     }
-
+    //get posts related
     public void searchPosts() {
-        String[] upperCasePostsNmaes = new String[posts.size()];   //coenvet everything in author to upper case
+        String[] upperCasePostsNames = new String[posts.size()];   //coenvet everything in author to upper case
         for (int i = 0; i < posts.size(); i++) {
-            upperCasePostsNmaes[i] = posts.get(i).getAuthor().toUpperCase();
+            upperCasePostsNames[i] = posts.get(i).getAuthor().toUpperCase();
         }
         System.out.println("Enter the name you are searching");     // check valid numbers are entered .
         String authorName = scan.nextLine().toUpperCase();
@@ -31,9 +32,29 @@ public class SearchCommentPost {
             authorName = scan.nextLine().toUpperCase();
         }
         for (int i = 0; i < posts.size(); i++) {                        //print out all the posts related with their title, name, and content. NO timestamps are included.
-            if (upperCasePostsNmaes[i].contains(authorName) && !authorName.equals(" ")) {
+            if (upperCasePostsNames[i].contains(authorName) && !authorName.equals(" ")) {
                 System.out.println("Author name: " + posts.get(i).getAuthor() + "\nTitle: " +
                         posts.get(i).getTitle() + "\nContent: " + posts.get(i).getContent());
+            }
+        }
+    }
+    //get comment related
+    public void searchComments() {
+        String[] upperCaseCommentsNames = new String[comments.size()];   //coenvet everything in author to upper case
+        for (int i = 0; i < comments.size(); i++) {
+            upperCaseCommentsNames[i] = comments.get(i).getAuthor().toUpperCase();                                        //***********************this not so sure becausse getText is not defined in local system
+        }
+        System.out.println("Enter the name you are searching");     // check valid numbers are entered .
+        String authorName = scan.nextLine().toUpperCase();
+        while (!authorName.matches("^[ A-Za-z]+$")) {
+            System.out.println("Only characters and space are allowed to enter!\n" +
+                    "Enter the name you are searching");
+            authorName = scan.nextLine().toUpperCase();
+        }
+        for (int i = 0; i < comments.size(); i++) {                        //print out all the posts related with their title, name, and content. NO timestamps are included.
+            if (upperCaseCommentsNames[i].contains(authorName) && !authorName.equals(" ")) {
+                System.out.println("Author name: " + comments.get(i).getAuthor() + "\nComment: " +
+                        comments.get(i).getContent());
             }
         }
     }
